8ca688064d19fda41c88be4584df95f9028d1c85
nguyenvuongviet <nguyenvuongviet2k4@gmail.com>
2024-12-22 22:39:08 +0700

    upsource

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import Example1 from "./example-1";
+import BaiTapThucHanhLayout from "./BaiTapLayoutComponent";
+
+function App() {
+  return (
+    <>
+      {/* <Example1 /> */}
+      <BaiTapThucHanhLayout />
+    </>
+  );
+}
+
+export default App;
---
+++ src/BaiTapLayoutComponent/index.jsx
@@ -0,0 +1,15 @@
+import Header from "./header";
+import Carousel from "./carousel";
+import Listcard from "./list-card";
+import Footer from "./footer";
+
+export default function BaiTapThucHanhLayout() {
+  return (
+    <div>
+      <Header />
+      <Carousel />
+      <Listcard />
+      <Footer />
+    </div>
+  );
+}
